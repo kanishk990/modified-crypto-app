f7c06da136e3e81f57dd5791dc74601bee2eea57
kanishk990 <80848509+kanishk990@users.noreply.github.com>
2022-08-29 16:14:51 +0530

    Api working now

--- src/component/Api.js
+++ src/component/Api.js
@@ -1,24 +1,50 @@
-import React, { useState, useEffect, } from 'react';
-import  ReactDOM  from 'react-dom';
+import React, {useState, useEffect} from 'react';
+import '../App.css';
 
-
-function Tabledata (){
-
-  const [objects, setObjects] = useState([]);
-
-  const fetchData = ()=>{
-
-    const apiurl = 'http://localhost:5000/getData';
-      fetch(apiurl)
-        .then((response) => response.json())
-        .then((data) => {
-          console.log(data);
-        });
+function InfofromApi (){
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/getData")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result);
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }, []);
+  console.log(items);
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return Object.keys(items).map((obj, i) =>{
+      return (
+        <div>
+          {items[obj].base_unit}
+          <div>
+            lowest is : {items[obj].low}
+          </div>
+          <div>
+            highest is : {items[obj].high}
+          </div>
+          <div>
+            latest is : {items[obj].last}
+            </div>
+        </div>
+      );
+    })
   }
-
-  useEffect( ()=>{
-    fetchData();
-  },[]);
-
 }
-export default Tabledata;
+
+export default InfofromApi;
